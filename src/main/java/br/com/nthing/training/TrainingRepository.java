2a5a5b27d017c9da9da03ec325817b8dbde82034
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-01-26 20:32:42 -0300

    Update TrainingRepository.java

--- src/main/java/br/com/nthing/training/TrainingRepository.java
+++ src/main/java/br/com/nthing/training/TrainingRepository.java
@@ -3,8 +3,24 @@ package br.com.nthing.training;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+
 @ApplicationScoped
 public class TrainingRepository implements PanacheRepository<Training> {
 
     //TODO Usar o update nativo do panache para teste
+    public List<Training> findByKeywordTraining(String keyword) {
+        return list("lower(training) like lower(?1)", "%" + keyword + "%");
+    }
+
+    public void updateTraining(Long id, Training training) {
+        Training entity = findById(id);
+        if (entity == null) {
+            throw new RuntimeException("Treino não encontrado");
+        }
+
+        String query = "UPDATE Training SET training = :nameTraining, totalTraining = :totalTraining, concludedAt = :concludedAt, status = :status, WHERE id = :id";
+        update(query, training.getNameTraining()); //TODO
+    }
+
 }
