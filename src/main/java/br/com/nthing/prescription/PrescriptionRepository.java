b1c7caaaee1aeab8dc1e91609c25a7904f95da2a
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-06-22 17:14:00 -0300

    Update PrescriptionRepository.java
    Create methods update and find keyword

--- src/main/java/br/com/nthing/prescription/PrescriptionRepository.java
+++ src/main/java/br/com/nthing/prescription/PrescriptionRepository.java
@@ -1,8 +1,21 @@
 package br.com.nthing.prescription;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
 
 @ApplicationScoped
 public class PrescriptionRepository implements PanacheRepository<Prescription> {
+
+    public List<Prescription> findKeywordPrescription(String keyword) {
+        return list("lower(namePrescription) like lower (?1)", "%" + keyword + "%");
+    }
+
+    public int updatePrescritption(Prescription prescription) {
+        return update("UPDATE Prescription p SET p.namePrescription = :namePrescription, WHERE p.id = :id",
+                Parameters.with("namePrescription", prescription.getNamePrescription())
+                        .and("id", prescription.getId()));
+    }
 }
